45e607cc3f22c9a72c711ac54dd9e7bdc4c85b19
Oleh Semennykov <sumcue@gmail.com>
2022-04-27 15:34:20 +0300

    add livepage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,85 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from 'nanoid';
+import React, { Component } from 'react';
+import FormByFormik from './Formik';
+// import Form from './Form/Form';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import s from './App.module.css';
+
+// export default function App() {
+//   return <FormByFormik />;
+// }
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = ({ id, name, number }) => {
+    const contact = {
+      name,
+      number,
+      id: nanoid(),
+    };
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  onChangeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContact = () => {
+    const { filter, contacts } = this.state;
+    const normalizeContact = filter.toLocaleLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeContact)
+    );
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('щбнвилсоь поле contacts');
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  render() {
+    const visibleContacts = this.getVisibleContact();
+    return (
+      <div className={s.container}>
+        <FormByFormik onSubmit={this.addContact} />
+        {/* <Form onSubmit={this.addContact} /> */}
+        <Filter value={this.state.filter} onChange={this.onChangeFilter} />
+        <span> Общее кол-во: {this.state.contacts.length}</span>
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;

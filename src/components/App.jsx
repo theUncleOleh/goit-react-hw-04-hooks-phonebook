c341aa46c58fb182804f62db828e750b936b1a86
Oleh Semennykov <sumcue@gmail.com>
2022-04-30 15:57:58 +0300

    delete ref

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 // import FormByFormik from './Formik';
 import Form from './Form/Form';
